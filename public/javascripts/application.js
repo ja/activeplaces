d4e1a429b469ce33e3c6d93d2468a8b9c94b1c13
James Andrews <me@jamesandre.ws>
2009-03-11 11:11:14 +0000

    Mini map for a particular site location.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,8 @@ var Map = function(mapContainerId, options) {
   var defaults = {
     locations : [],
     mapCenter : null,
-    mapZoom : 14,
-    admin: false
+    mapZoom   : 14,
+    admin     : false
   }
   this.options = $.extend(defaults, options);
   
@@ -21,11 +21,13 @@ $.extend(Map.prototype, {
 
   _setup: function(mapContainerId, options) {
     this.gmap = new GMap2(document.getElementById(mapContainerId));
+
     if (options.mapCenter) {
       var center = new GLatLng(options.mapCenter[0], options.mapCenter[1]);
     } else if (options.locations.length > 0) {
-      var center = new GLatLng(options.locations[0].location.latitude, options.locations[0].location.longitude);
+      var center = new GLatLng(options.locations[0].site.latitude, options.locations[0].site.longitude);
     }
+
     this.gmap.setCenter(center, options.mapZoom);
     this.gmap.addControl(new GSmallMapControl());
     this.gmap.addControl(new GHierarchicalMapTypeControl());
